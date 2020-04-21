6e178aacb0e14e477685e6ce918ff81a18066751
Brennon Nuckols <brennuck@gmail.com>
2020-04-21 13:03:15 -0600

    STEP 2

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import axios from 'axios';
+import UserCard from './Components/UserCard';
 
 class App extends React.Component {
-  state = {
-    users: []
-  };
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
 
   componentDidMount() {
     axios.get("https://api.github.com/users/brennuck")
@@ -23,7 +27,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <UserCard users={this.state.users} />
       </div>
     )
   }
---
+++ usercard/src/Components/UserCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserCard = props => {
+    console.log("PROPS", props)
+    return (
+        <div>
+            <img src={props.users.avatar_url} />
+            <h2> {props.users.name} </h2>
+            <p> {props.users.bio} </p>
+            <h4> {props.users.blog} </h4>
+        </div>
+    )
+}
+
+export default UserCard;
